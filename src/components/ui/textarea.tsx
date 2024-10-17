ab1e68b69c6b1f02a9a71335740c59e4737db5e6
Uilian Comim <ucomim@hotmail.com>
2024-10-17 20:06:57 -0300

    refactor: fix dark theme for components

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+    ({ className, ...props }, ref) => {
+        return (
+            <textarea
+                className={cn(
+                    "flex min-h-16 w-full rounded-md border border-gray-400 bg-transparent px-3 py-2 text-sm text-gray-800 dark:text-gray-300 shadow-sm transition-colorsdisabled:cursor-not-allowed disabled:opacity-50",
+                    className
+                )}
+                ref={ref}
+                {...props}
+            />
+        );
+    }
+);
+Textarea.displayName = "Textarea";
+
+export { Textarea };
