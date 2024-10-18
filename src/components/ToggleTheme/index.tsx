15fe62183f0ec825610fa910e83785462d3da653
Uilian Comim <ucomim@hotmail.com>
2024-10-18 10:27:22 -0300

    feat: add alternative screen if not exists task

--- src/components/ToggleTheme/index.tsx
+++ src/components/ToggleTheme/index.tsx
@@ -1,21 +1,34 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Toggle } from "../ui/toggle";
 import { Sun, Moon } from "lucide-react";
 
 export function ToggleTheme() {
     const [theme, setTheme] = useState<"light" | "dark">("light");
+
+    useEffect(() => {
+        const currentTheme = localStorage.getItem("theme");
+
+        if (currentTheme) {
+            setTheme(currentTheme as "light" | "dark");
+            document.documentElement.setAttribute("data-theme", currentTheme);
+        }
+    }, []);
+
     const toggleTheme = useCallback(() => {
         const currentTheme =
             document.documentElement.getAttribute("data-theme");
         if (currentTheme === "dark") {
             document.documentElement.setAttribute("data-theme", "light");
             setTheme("light");
+            localStorage.setItem("theme", "light");
         } else {
             document.documentElement.setAttribute("data-theme", "dark");
             setTheme("dark");
+            localStorage.setItem("theme", "dark");
         }
     }, []);
+
     return (
         <Toggle
             className="absolute top-5 right-5 text-gray-600"
