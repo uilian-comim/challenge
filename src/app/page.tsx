a36d44aea9dc21c88683aa86d925dee54fdd3448
Uilian Comim <ucomim@hotmail.com>
2024-10-17 22:55:28 -0300

    feat: add components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { Main } from "@/components/Main";
+import { ToggleTheme } from "@/components/ToggleTheme";
+import { CreateTaskDialog } from "./CreateTaskDialog";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
-    return <p>Hello world</p>;
+    return (
+        <Main>
+            <ToggleTheme />
+            <div className="flex items-center justify-center gap-2">
+                <Input name="teste" />
+                <div>
+                    <CreateTaskDialog />
+                </div>
+            </div>
+        </Main>
+    );
 }
