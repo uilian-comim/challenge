9b5b51707a8f3c19d3de4ff857032ad61bce6302
Uilian Comim <ucomim@hotmail.com>
2024-10-18 09:57:02 -0300

    refactor: rebuilt page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import { Main } from "@/components/Main";
 import { ToggleTheme } from "@/components/ToggleTheme";
 import { CreateTaskDialog } from "./CreateTaskDialog";
-import { Input } from "@/components/ui/input";
+import { RenderTasks } from "./RenderTask";
+import { SearchTaskFilter } from "./SearchTaskFilter";
+import { FilterTaskDialog } from "./FilterTaskDialog";
 
 export default function Home() {
     return (
-        <Main>
+        <Main className="pt-20 gap-3">
             <ToggleTheme />
-            <div className="flex items-center justify-center gap-2">
-                <Input name="teste" />
-                <div>
+            <div className="w-full flex items-center justify-center gap-2">
+                <SearchTaskFilter />
+                <div className="flex items-center justify-center gap-2">
+                    <FilterTaskDialog />
                     <CreateTaskDialog />
                 </div>
             </div>
+            <RenderTasks />
         </Main>
     );
 }
