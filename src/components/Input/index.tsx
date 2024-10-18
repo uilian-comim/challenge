18ed0e152747af39cd2bf48eebd262e92e83d1b7
Uilian Comim <ucomim@hotmail.com>
2024-10-17 22:53:04 -0300

    feat: FormInput component finalized

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,9 @@
 import { cva, VariantProps } from "class-variance-authority";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { cn } from "@/lib/utils";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { Span } from "../Span";
 
 const inputVariants = cva("", {
     variants: {
@@ -19,34 +22,59 @@ const inputVariants = cva("", {
     },
 });
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
-    VariantProps<typeof inputVariants> & {
-        name: string;
-        label?: string;
-        labelClassName?: string;
-    };
+const labelVariants = cva(
+    "text-gray-800 dark:text-gray-300 text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+);
 
-export function FormInput({
+type InputProps<T extends FieldValues> =
+    React.InputHTMLAttributes<HTMLInputElement> &
+        VariantProps<typeof inputVariants> & {
+            name: Path<T>;
+            label?: string;
+            labelClassName?: string;
+            control: Control<T, unknown>;
+            error?: string;
+        };
+
+export function FormInput<T extends FieldValues>({
     name,
     label,
     className,
     labelClassName,
     variant,
     inputSize,
+    control,
+    error,
     ...rest
-}: InputProps) {
+}: InputProps<T>) {
     return (
-        <div className="flex flex-col items-center justify-start gap-1">
+        <div className="w-full flex flex-col items-start justify-center gap-1">
             {label && (
-                <Label htmlFor={name} className={labelClassName}>
+                <Label
+                    htmlFor={name}
+                    className={cn(labelVariants({ className: labelClassName }))}
+                >
                     {label}
                 </Label>
             )}
-            <Input
+            <Controller
+                control={control}
                 name={name}
-                className={inputVariants({ className, variant, inputSize })}
-                {...rest}
+                render={({ field }) => (
+                    <Input
+                        {...field}
+                        {...rest}
+                        className={cn(
+                            inputVariants({ variant, inputSize, className })
+                        )}
+                    />
+                )}
             />
+            {error && (
+                <Span className="text-red-500 text-xs font-medium">
+                    {error}
+                </Span>
+            )}
         </div>
     );
 }
