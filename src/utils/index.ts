51e3d5f08c14bb0a68222617b0d9f21e52442a5e
Uilian Comim <ucomim@hotmail.com>
2024-10-17 22:51:20 -0300

    feat: create utils file

---
+++ src/utils/index.ts
@@ -0,0 +1,11 @@
+import format from "./format";
+import LS from "./localStorage";
+import schemas from "./schemas";
+
+const utils = {
+    schemas,
+    format,
+    LS,
+};
+
+export default utils;
